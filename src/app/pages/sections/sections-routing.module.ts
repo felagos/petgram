23ec79a316a89dfbfb5f7caf30cc3c0f1d79417a
Felipe Lagos <felagossanchez@gmail.com>
2020-06-22 19:10:58 -0400

    first commit

---
+++ src/app/pages/sections/sections-routing.module.ts
@@ -0,0 +1,41 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { SectionsPage } from './sections.page';
+
+const routes: Routes = [
+  {
+    path: "sections",
+    component: SectionsPage,
+    children: [
+      {
+        path: "home",
+        loadChildren: () => import("@pages/home/home-routing.module").then(m => m.HomePageRoutingModule)
+      },
+      {
+        path: "favorites",
+        loadChildren: () => import("@pages/favorites/favorites-routing.module").then(m => m.FavoritesPageRoutingModule)
+      },
+      {
+        path: "auth",
+        loadChildren: () => import("@pages/auth/auth-routing.module").then(m => m.AuthPageRoutingModule)
+      },
+      {
+        path: "",
+        pathMatch: "full",
+        redirectTo: "sections/home"
+      }
+    ]
+  },
+  {
+    path: "",
+    pathMatch: "full",
+    redirectTo: "sections/home"
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class SectionsPageRoutingModule { }

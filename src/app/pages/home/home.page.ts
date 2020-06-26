7cdbc8ea86bdd035392264413196193f904d2899
Felipe Lagos <felagossanchez@gmail.com>
2020-06-25 21:26:19 -0400

    servicio para api rest

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/services/api.service';
+import { Category } from 'src/app/models/category.model';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePage implements OnInit {
 
-  constructor() { }
+  public categories: Category[] = [];
+
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+    this.apiService.getAllCategories().subscribe(response => this.categories = response);
   }
 
 }

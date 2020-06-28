0c3c39fbe6ce6f86c3ac7aadcc45cb35e1c56b08
Felipe Lagos <felagossanchez@gmail.com>
2020-06-28 15:18:41 -0400

    redirtect de http to https

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 const express = require("express");
-
+var enforce = require("express-sslify");
 const app = express();
 
 app.use(express.static("./www"));
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.get("/*", (req, res) =>
     res.sendFile("index.html", { root: "www/" }),
 );
 
-app.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https') {
-        res.redirect(`https://${req.header('host')}${req.url}`)
-    } else {
-        next();
-    }
-});
 
 app.listen(process.env.PORT || 8080, () => {
     console.log("server up !");

f6c169556ffc145dad65a93fe2596fc6571865ba
Felipe Lagos <felagossanchez@gmail.com>
2020-06-28 15:11:49 -0400

    redirtect de http to https

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ app.get("/*", (req, res) =>
     res.sendFile("index.html", { root: "www/" }),
 );
 
+app.use((req, res, next) => {
+    if (req.header('x-forwarded-proto') !== 'https') {
+        res.redirect(`https://${req.header('host')}${req.url}`)
+    } else {
+        next();
+    }
+});
+
 app.listen(process.env.PORT || 8080, () => {
     console.log("server up !");
 });

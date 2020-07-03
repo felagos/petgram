fd38c4a8822bacba427390b4c21bd27b37846eb1
Felipe Lagos <felagossanchez@gmail.com>
2020-07-03 13:17:31 -0400

    funcion para obtener usuario del token

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Plugins, StoragePlugin } from '@capacitor/core';
+import { StorageEnum } from '../enums/storage.enum';
+import { TokenResponse } from '../models/token.response';
+import { UserModel } from '../models/user.model';
 
 @Injectable({
   providedIn: 'root'
@@ -23,6 +26,16 @@ export class StorageService {
   public async getItem<T>(key: string): Promise<T> {
     const item = await this.storage.get({ key });
     return JSON.parse(item.value);
+  }
+
+  public async getUser(): Promise<UserModel> {
+    const tokens = await this.getItem<TokenResponse>(StorageEnum.TOKEN);
+    const token = tokens.token;
+    const sectionsToken = token.split(".");
+    const payload: any = atob(sectionsToken[1]);
+    const data = JSON.parse(payload);
+
+    return data.user as UserModel;
   }
 
 }

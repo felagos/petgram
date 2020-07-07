4fa79c3628fcca8628925f98bc0b538c5b3163fb
Felipe Lagos <felagossanchez@gmail.com>
2020-07-06 20:30:52 -0400

    guardo el token y refresh token en cosntantes separadas

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -58,7 +58,8 @@ export class AuthPage implements OnInit {
     await this.loaderService.present();
 
     this.authService.doLogin(email, password).subscribe(async response => {
-      this.storage.setItem(StorageEnum.TOKEN, response.data);
+      this.storage.setItem(StorageEnum.TOKEN, response.data.token);
+      this.storage.setItem(StorageEnum.REFRESH_TOKEN, response.data.refreshToken);
       await this.loaderService.dismiss();
 
       this.dataService.addData(true);

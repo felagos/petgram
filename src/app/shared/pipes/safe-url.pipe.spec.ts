749fa48a1f3d001bf0d321cbcb2f9e3f8190d97f
Felipe Lagos <felagossanchez@gmail.com>
2020-07-23 17:17:21 -0400

    ajuste pu pipe

--- src/app/shared/pipes/safe-url.pipe.spec.ts
+++ src/app/shared/pipes/safe-url.pipe.spec.ts
@@ -1,8 +1,27 @@
 import { SafeUrlPipe } from './safe-url.pipe';
+import { TestBed } from '@angular/core/testing';
+import { DomSanitizer } from '@angular/platform-browser';
 
-describe('SafeUrlPipe', () => {
+fdescribe('SafeUrlPipe', () => {
+  let pipe: SafeUrlPipe;
+
+  beforeEach(async () => {
+    TestBed.configureTestingModule({
+      providers: [ {provide: DomSanitizer, useValue: { bypassSecurityTrustResourceUrl: () => 'safeUrl' } } ]
+    });
+    const sanitized = TestBed.get(DomSanitizer);
+    pipe = new SafeUrlPipe(sanitized);
+  });
+
   it('create an instance', () => {
-    const pipe = new SafeUrlPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it("transform", () => {
+    const value = String(Date.now());
+    const response = pipe.transform(value);
+
+    expect(response).toEqual("safeUrl");
+  });
+
 });

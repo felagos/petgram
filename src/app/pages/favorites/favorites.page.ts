f8bdf560f0b6c09a64140017b0df3c5ac11721b8
Felipe Lagos <felagossanchez@gmail.com>
2020-08-22 17:28:58 -0400

    accion para borrar desde favoritos

--- src/app/pages/favorites/favorites.page.ts
+++ src/app/pages/favorites/favorites.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { ApiService } from 'src/app/services';
+import { ApiService, ToastService } from 'src/app/services';
 import { PetModel } from 'src/app/models';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-favorites',
@@ -11,10 +12,36 @@ export class FavoritesPage {
 
   public pets: PetModel[] = [];
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService,
+    private toastService: ToastService,
+    private alertController: AlertController) { }
 
   ionViewDidEnter() {
     this.apiService.getAllFavorities().subscribe(pets => this.pets = pets.data);
+  }
+
+  async deleteFavorite(id: string) {
+    const alert = await this.alertController.create({
+      header: 'Confirmar',
+      message: '¿ Desea quitarlo desde sus favoritos ?',
+      buttons: [
+        {
+          text: 'Aceptar',
+          handler: () => {
+            this.apiService.deleteFavorite(id).subscribe(() => {
+              this.toastService.presentToast("Se ha quitado de los favoritos");
+            });
+          }
+        },
+        {
+          text: 'Cancelar',
+          role: 'cancel'
+        },
+      ]
+    });
+
+    await alert.present();
+
   }
 
 }

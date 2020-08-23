f779aecc9047cad9cb976e1483cebe24044c0b09
Felipe Lagos <felagossanchez@gmail.com>
2020-08-23 18:33:16 -0400

    pipe para obtener foto de perfil

--- src/app/shared/pipes/photo-profile.pipe.spec.ts
+++ src/app/shared/pipes/photo-profile.pipe.spec.ts
@@ -3,31 +3,23 @@ import { UserModel } from 'src/app/models';
 
 fdescribe('PhotoProfilePipe', () => {
   const pipe = new PhotoProfilePipe();
-  
+
   it('create an instance', () => {
     expect(pipe).toBeTruthy();
   });
 
   it('return empty photo', () => {
-    const user: UserModel = {
-      email: "",
-      nombre: "",
-      foto: ""
-    };
-    const response = pipe.transform(user);
+    const foto = "";
+    const response = pipe.transform(foto);
     expect(response).toEqual("assets/imgs/no_user.png");
   });
 
   it('return photo', () => {
-    const user: UserModel = {
-      email: "",
-      nombre: "",
-      foto: "la foto"
-    };
+    const foto = "la foto";
 
-    const base64Img = `data:image/png;base64, ${user.foto}`;
+    const base64Img = `data:image/png;base64, ${foto}`;
 
-    const response = pipe.transform(user);
+    const response = pipe.transform(foto);
     expect(response).toEqual(base64Img);
   });
 
--- src/app/shared/pipes/photo-profile.pipe.ts
+++ src/app/shared/pipes/photo-profile.pipe.ts
@@ -1,14 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { UserModel } from 'src/app/models';
 
 @Pipe({
   name: 'photoProfile'
 })
 export class PhotoProfilePipe implements PipeTransform {
 
-  transform(user: UserModel): string {
-    if(!user || !user.foto) return "assets/imgs/no_user.png";
-    return `data:image/png;base64, ${user.foto}`;
+  transform(foto :string): string {
+    if(!foto) return "assets/imgs/no_user.png";
+    return `data:image/jpeg;base64, ${foto}`;
   }
 
 }

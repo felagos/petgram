e694b870ad52bb87c16cab7a100dab6c8cd98daa
Felipe Lagos <felagossanchez@gmail.com>
2020-08-27 23:46:47 -0400

    prueba unitaria servicio camara

--- src/app/services/camera.service.spec.ts
+++ src/app/services/camera.service.spec.ts
@@ -1,8 +1,8 @@
 import { TestBed } from '@angular/core/testing';
-
 import { CameraService } from './camera.service';
+import { Plugins } from '@capacitor/core';
 
-describe('CameraService', () => {
+fdescribe('CameraService', () => {
   let service: CameraService;
 
   beforeEach(() => {
@@ -13,4 +13,18 @@ describe('CameraService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it("take photo", async () => {
+    const { Camera } = Plugins;
+    const photoOption = {
+      format: "",
+      base64String: ""
+    };
+    spyOn(Camera, "getPhoto").and.resolveTo(photoOption);
+
+    const photo = await service.getPhoto();
+    
+    expect(photo).toEqual(photoOption);
+  });
+
 });

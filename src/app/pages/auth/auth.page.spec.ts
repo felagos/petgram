785499b856589ca830455acace0e4eccd65d2749
Felipe Lagos <felagossanchez@gmail.com>
2020-10-06 23:25:21 -0300

    pu pagina login

--- src/app/pages/auth/auth.page.spec.ts
+++ src/app/pages/auth/auth.page.spec.ts
@@ -1,16 +1,30 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { RouterTestingModule } from '@angular/router/testing';
+import { IonicModule, ModalController } from '@ionic/angular';
+import { AuthService, LoaderService, LoggedDataService, StorageService, ToastService } from 'src/app/services';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { AuthPage } from './auth.page';
 
-describe('AuthPage', () => {
+fdescribe('AuthPage', () => {
   let component: AuthPage;
   let fixture: ComponentFixture<AuthPage>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AuthPage ],
-      imports: [IonicModule.forRoot()]
+      declarations: [AuthPage],
+      imports: [IonicModule.forRoot(), ReactiveFormsModule, FormsModule, RouterTestingModule, HttpClientTestingModule],
+      providers:[
+        FormBuilder,
+        ModalController,
+        LoaderService,
+        StorageService,
+        AuthService,
+        ToastService,
+        LoggedDataService
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(AuthPage);
@@ -21,4 +35,17 @@ describe('AuthPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('invalid form', () => {
+    const email = component.loginForm.controls["email"];
+    const password = component.loginForm.controls["password"];
+
+    email.setValue("");
+    password.setValue("");
+
+    expect(email.valid).toBeFalse();
+    expect(password.valid).toBeFalse();
+    expect(component.loginForm.valid).toBeFalse();
+  });
+
 });

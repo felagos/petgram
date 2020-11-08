525c68d684e9c58ae0a87aa598c85080597e4902
Felipe Lagos <felagossanchez@gmail.com>
2020-11-08 12:15:53 -0300

    ajustes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: "https://petgram-backend.herokuapp.com"
+  API_URL: "https://backend.felipelagos.dev/petgram"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: "https://petgram-backend.herokuapp.com"
+  API_URL: "https://backend.felipelagos.dev/petgram"
 };
 
 /*
